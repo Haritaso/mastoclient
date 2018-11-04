ceaec3033f45125a9d812afd2606ca219f4f9876
Haritaso <mm.620@icloud.com>
2018-11-05 03:29:18 +0900

    tootbar画像投稿対応

--- components/simpletootbar.vue
+++ components/simpletootbar.vue
@@ -31,8 +31,50 @@
         >
           <el-input class="cwinput" v-show="cwtoggle" placeholder="cw" size="medium" v-model="toot.cwContent"></el-input>
           <div class="optionbtn">
-            <el-button size="medium" type="info" icon="el-icon-view" circle @click="cwtoggle = !cwtoggle"></el-button>
-            <el-button size="medium" type="info" icon="el-icon-picture-outline" circle></el-button>
+            <div class="optionbtn1">
+              <div><el-button size="medium" type="info" icon="el-icon-view" circle @click="cwtoggle = !cwtoggle"></el-button></div>
+              <el-upload
+                class=""
+                :action="pictureUrl"
+                :headers="pictureHeader"
+                :show-file-list="false"
+                multiple
+                :limit="4"
+                :on-change="pictureAdd"
+                :on-success="pictureSuc"
+                :file-list="toot.picture"
+                list-type="picture"
+              >
+                <el-button
+                  :loading="loading"
+                  size="medium"
+                  type="info"
+                  icon="el-icon-picture-outline"
+                  class="btn"
+                  circle
+                ></el-button>
+              </el-upload>
+              <div v-show="nsfw">
+                <el-button
+                  size="medium"
+                  type="info"
+                  :icon="nsfwmode"
+                  circle
+                  @click="toot.sensitive = !toot.sensitive"
+                  class="btn"
+                >
+                </el-button>
+              </div>
+            </div>
+            <div class="preimg">
+              <img
+                v-if="mediacount > 0"
+                class="tootbarpre"
+                v-for="(item, i) in filelist"
+                :key="i"
+                :src="item.url"
+              >
+            </div>
           </div>
           <el-button class="option" type="mini" slot="reference">Option</el-button>
         </el-popover>
@@ -79,16 +121,46 @@ export default {
         cwContent: '',
         sensitive: false,
         media: [],
-        imageurl: ''
       },
       cwtoggle: false,
+      nsfw: false,
+      loading: false,
+      filelist: [],
+      first: false,
+      mediacount: 0,
       styles
     }
   },
   computed: {
     id () {
       return this.$store.getters.getid
+    },
+    token () {
+      return this.first ? this.$store.getters.getactive[0].accessToken : "dummy"
+    },
+    pictureUrl() {
+      return "https://" + this.$store.getters.geturl + "/api/v1/media"
+    },
+    pictureHeader() {
+      return {
+        Authorization: "Bearer " + this.token
+      }
+    },
+    nsfwmode () {
+      let a = ""
+      if (this.toot.sensitive == false) {
+        a = "el-icon-question"
+      } else {
+        a = "el-icon-warning"
+      }
+      return a
     }
+  },
+  mounted () {
+    setTimeout(() => {
+      this.first = true
+      this.loading = false
+    }, 1)
   },
   methods: {
     tootaction () {
@@ -98,12 +170,38 @@ export default {
       this.toot.TootContent = ''
       this.toot.cwContent = ''
       this.toot.media = []
+      this.filelist = []
+      this.nsfw = true
+    },
+    pictureAdd(file, filelist) {
+      this.filelist = filelist
+      this.loading = true
+    },
+    pictureSuc(response, file, filelist) {
+      this.filelist = filelist
+      this.toot.media.push(filelist[this.mediacount].response.id)
+      this.mediacount++
+      this.nsfw = true
+      this.$nextTick(() => {
+        this.loading = false
+        this.toot.media = this.toot.media
+      })
     }
   }
 }
 </script>
 
 <style scope>
+.tootbarpre {
+  width: 36px;
+  height: 36px;
+  display: block;
+  border-radius: 50%;
+}
+.preimg {
+  float: right;
+  display: inline-flex;
+}
 .el-select .el-input {
   width: 95px;
 }
@@ -130,26 +228,25 @@ export default {
   width: -webkit-fill-available;
 }
 .optionbtn {
-  min-width: 100px;
   max-height: 36px;
-  margin: auto 6px auto auto;
+  display: block;
+}
+.optionbtn1 {
   display: inline-flex;
-  justify-content: space-around;
+  float: left;
 }
 .tootbar {
-  z-index: 100!important;
+  z-index: 100 !important;
   max-height: 130px;
   min-height: 120px;
   margin-right: 15px;
 }
 .window {
-  border-radius: 4px!important;
-  position: fixed!important;
-  
+  border-radius: 4px !important;
+  position: fixed !important;
 }
-.window .titlebar{
-  border-radius: 0px!important;
-  
+.window .titlebar {
+  border-radius: 0px !important;
 }
 .title {
   font-size: 12px;
